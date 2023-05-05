973faeccc7219b788fabb36b2904853b4ce6dec1
zw <zw@163.com>
2023-05-05 18:12:42 +0800

    zw first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,5 +3,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello Git! zw master second commit");
     }
 }
