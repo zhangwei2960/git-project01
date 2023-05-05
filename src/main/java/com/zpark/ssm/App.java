372a7dca70d9abb4f40ac0e474863c538e38e6fd
zw <zw@163.com>
2023-05-05 18:15:44 +0800

    zw master third commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello Git! zw master second commit");
+        System.out.println("hello Git! zw master third commit");
     }
 }
