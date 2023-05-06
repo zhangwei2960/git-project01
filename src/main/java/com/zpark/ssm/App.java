26724f29d148a92e7ed2096b54ffd8d4848c98e3
zw <zw@163.com>
2023-05-06 09:10:48 +0800

    zw hotfix first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("hello git");
         System.out.println("hello Git! zw master second commit");
         System.out.println("hello Git! zw master third commit");
+        System.out.println("hello Git! zw hotfix first commit!");
     }
 }
